378d94de33351676b777d1d5ef373920cfa5375f
Kyle Skelly <kyle.skelly32@gmail.com>
2017-03-26 00:21:54 +0000

    added code comments

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'Skellprograms@gmail.com'
+  default to: 'skellprograms@gmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
