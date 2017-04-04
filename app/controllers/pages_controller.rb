f6efe462bb5a8c1cc826137f3a4e222dfc45ac66
Kyle Skelly <kyle.skelly32@gmail.com>
2017-04-04 21:45:56 +0000

    Added conditional logic on signup form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
     #get request for / which is our homepage
     def home
         @basic_plan = Plan.find(1)
-        @pro_plan = Plan.find(2) # Is this 
+        @pro_plan = Plan.find(2)
     end
     
     def about
